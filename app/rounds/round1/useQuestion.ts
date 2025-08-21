4b499dbbe08f1ce77d53dd610ba0edfd49a2f5e9
KowaiPanda <pokemongojr011@gmail.com>
2025-08-22 00:42:44 +0530

    change imports

--- app/rounds/round1/useQuestion.ts
+++ app/rounds/round1/useQuestion.ts
@@ -47,7 +47,7 @@ export default function useQuestion() {
           return;
         }
 
-        if (!response.error) {
+        if (response.error) {
           throw new Error(response.error || 'Failed to fetch question');
         }
 
